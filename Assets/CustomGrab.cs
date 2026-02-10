3fb1d70b8ef40e2c0d50317dc8e0474b5530ecc7
Emma0202-hash <evirtane22@student.oulu.fi>
2026-02-10 13:30:48 +0200

    Magnifier DONE - Two hand grabbing in progress

--- Assets/CustomGrab.cs
+++ Assets/CustomGrab.cs
@@ -5,7 +5,8 @@ using UnityEngine.InputSystem;
 
 public class CustomGrab : MonoBehaviour
 {
-    // Make sure to define the input in the editor (LeftHand/Grip and RightHand/Grip recommended respectively)
+    // TEHTY This script should be attached to both controller objects in the scene
+    // TEHTY Make sure to define the input in the editor (LeftHand/Grip and RightHand/Grip recommended respectively)
 
     CustomGrab otherHand = null; // Tallennetaan toisen ohjaimen skripti, jotta kädet voivat "jakaa" objektin
     public List<Transform> nearObjects = new List<Transform>(); // Lista lähellä olevista objekteista
@@ -13,136 +14,60 @@ public class CustomGrab : MonoBehaviour
     public InputActionReference action; // Unityn Input System-toiminto
     bool grabbing = false; // Tarkistaa onko nappi pohjassa
 
-    // Edellisen framen sijainti ja rotaatio
-    Vector3 lastPos;
-    Quaternion lastRot;
-
-    // Apumuuttuja, tarkistaa onko tämä käsi tarttumassa (kahden käden logiikkaa varten)
-    public bool IsGrabbing => grabbedObject != null;
+    Vector3 originalLocation; // Objektin alkuperäinen sijainti
+    Quaternion originalRotation; // Objektin alkuperäinen rotaatio
 
     private void Start()
     {
         action.action.Enable(); // Aktivoi napin, jotta sitä voidaan lukea
 
-        // Etsitään toinen käsi -> etsii kaikki CustomGrab-skriptit parentin alta
+        // Find the other hand
         foreach (CustomGrab c in transform.parent.GetComponentsInChildren<CustomGrab>())
         {
             if (c != this)
                 otherHand = c;
         }
-
-        // Tallennetaan käden alkuasento. Voidaan verrata nykyistä sijaintia tähän
-        lastPos = transform.position;
-        lastRot = transform.rotation;
     }
 
     void Update()
     {
-        // Lasketaan delta-liikkeet
-        Vector3 deltaPos = transform.position - lastPos;
-        Quaternion deltaRot = transform.rotation * Quaternion.Inverse(lastRot);
-
-        // Tarkistetaan onko nappi pohjassa
-        grabbing = action.action.IsPressed();
-
+        grabbing = action.action.IsPressed(); // Tarkistetaan onko nappi pohjassa
         if (grabbing)
         {
-            // Jos toinen käsi pitää jo objektia -> tartu samaan objektiin
+            // Grab nearby object or the object in the other hand
             if (!grabbedObject)
-            {
-             // grabbedObject = nearObjects.Count > 0 ? nearObjects[0] : otherHand.grabbedObject;
-             if (otherHand.IsGrabbing)
-             {
-                 grabbedObject = otherHand.grabbedObject;
-             }
-             // Muuten tartu lähimpään objektiin
-             else if (nearObjects.Count > 0)
-             {
-                 grabbedObject = nearObjects[0];
-             }
-          }
-            // Jos jokin objekti on valittu
+                grabbedObject = nearObjects.Count > 0 ? nearObjects[0] : otherHand.grabbedObject;
+
             if (grabbedObject)
             {
-                // --- YHDEN KÄDEN LOGIIKKA ---
-                if (!otherHand.IsGrabbing || otherHand.grabbedObject != grabbedObject)
-                {
-                    ApplyOneHand(deltaPos, deltaRot);
-                }
-                // --- KAHDEN KÄDEN LOGIIKKA ---
-                else
-                {
-                    ApplyTwoHands(deltaPos, deltaRot);
-                }
+                // Change these to add the delta position and rotation instead
+                // Save the position and rotation at the end of Update function, so you can compare previous pos/rot to current here
+                grabbedObject.position = transform.position;
+                grabbedObject.rotation = transform.rotation;
             }
         }
-        // Jos nappi vapautetaan → päästetään irti
+        // If let go of button, release object
         else if (grabbedObject)
-        {
             grabbedObject = null;
-        }
 
-        // Tallennetaan nykyinen sijainti ja rotaatio seuraavaa framea varten
-        lastPos = transform.position;
-        lastRot = transform.rotation;
+        // Should save the current position and rotation here
+
+
     }
-
-    // YHDEN KÄDEN DELTA-LIIKE
-    void ApplyOneHand(Vector3 deltaPos, Quaternion deltaRot)
-    {
-        // 1. Objekti liikkuu saman verran kuin käsi liikkuu (translation)
-        grabbedObject.position += deltaPos;
-
-        // 2. Objekti pyörii saman verran kuin käsi pyörii (rotation causes translation)
-        Vector3 offset = grabbedObject.position - transform.position;
-        offset = deltaRot * offset;
-        grabbedObject.position = transform.position + offset;
-
-        // 3. Objekti kiertää kättä, jos käsi pyörii = orbit-liike
-        grabbedObject.rotation = deltaRot * grabbedObject.rotation;
-    }
-
-    // KAHDEN KÄDEN DELTA-LIIKE
-    void ApplyTwoHands(Vector3 myDeltaPos, Quaternion myDeltaRot)
-    {
-        // Toisen käden delta-liike
-        Vector3 otherDeltaPos = otherHand.transform.position - otherHand.lastPos;
-        Quaternion otherDeltaRot = otherHand.transform.rotation * Quaternion.Inverse(otherHand.lastRot);
-
-        // Yhdistetään liikkeet
-        Vector3 combinedPos = myDeltaPos + otherDeltaPos;
-        Quaternion combinedRot = myDeltaRot * otherDeltaRot;
-
-        // 1. Yhdistetty translation
-        grabbedObject.position += combinedPos;
-
-        // 2. Orbit-liike (rotaatio aiheuttaa liikettä)
-        Vector3 offset = grabbedObject.position - transform.position;
-        offset = combinedRot * offset;
-        grabbedObject.position = transform.position + offset;
-
-        // 3. Yhdistetty rotaatio
-        grabbedObject.rotation = combinedRot * grabbedObject.rotation;
-    }
-
-    // Kun ohjain koskettaa jotain
+        // Kun ohjain koskettaa jotain
     private void OnTriggerEnter(Collider other)
     {
-        // TEHTY Make sure to tag grabbable objects with the "grabbable" tag
-        // TEHTY You also need to make sure to have colliders for the grabbable objects and the controllers
-            // Magnifier Box Collider
-            // Ohjaimet Sphere Collider
-        // TEHTY Make sure to set the controller colliders as triggers or they will get misplaced
-        // TEHTY You also need to add Rigidbody to the controllers for these functions to be triggered
-        // TEHTY Make sure gravity is disabled though, or your controllers will (virtually) fall to the ground
+        // Make sure to tag grabbable objects with the "grabbable" tag
+        // You also need to make sure to have colliders for the grabbable objects and the controllers
+        // Make sure to set the controller colliders as triggers or they will get misplaced
+        // You also need to add Rigidbody to the controllers for these functions to be triggered
+        // Make sure gravity is disabled though, or your controllers will (virtually) fall to the ground
 
-        Debug.Log("Trigger entered: " + other.name);
         Transform t = other.transform;
         if (t && t.tag.ToLower() == "grabbable")
             nearObjects.Add(t);
     }
-
-    // Kun objekti poistuu läheltä
+        // Kun objekti poistuu läheltä
     private void OnTriggerExit(Collider other)
     {
         Transform t = other.transform;
